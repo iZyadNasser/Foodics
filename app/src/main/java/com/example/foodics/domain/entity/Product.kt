8ddf8a88e6a728d9058db555d71e1a1b0ade2358
Zyad <zyadhammad531@gmail.com>
2026-01-18 21:38:13 +0200

    feat: add project entities - category - product

---
+++ app/src/main/java/com/example/foodics/domain/entity/Product.kt
@@ -0,0 +1,12 @@
+package com.example.foodics.domain.entity
+
+import java.util.UUID
+
+data class Product(
+    val id: UUID,
+    val name: String,
+    val category: Category,
+    val description: String?,
+    val imageUrl: String,
+    val price: Double,
+)
