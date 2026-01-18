077b07b009a152cc2acbd54d765c9c6bacad9094
Zyad <zyadhammad531@gmail.com>
2026-01-18 21:15:56 +0200

    feat: setup koin di

---
+++ app/src/main/java/com/example/foodics/App.kt
@@ -0,0 +1,16 @@
+package com.example.foodics
+
+import android.app.Application
+import com.example.foodics.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/foodics/di/useCaseModule.kt
@@ -0,0 +1,7 @@
+package com.example.foodics.di
+
+import org.koin.dsl.module
+
+val useCaseModule = module {
+    // TODO
+}
