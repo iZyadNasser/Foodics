7d9ef3478bc13a1576eda48b2f98b460ebcb4340
Zyad <zyadhammad531@gmail.com>
2026-01-20 21:13:40 +0200

    feat: add alert to application

--- app/src/main/java/com/example/foodics/ui/feature/tables/TablesRoot.kt
+++ app/src/main/java/com/example/foodics/ui/feature/tables/TablesRoot.kt
@@ -2,7 +2,10 @@ package com.example.foodics.ui.feature.tables
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.foodics.ui.LocalErrorAlert
+import com.example.foodics.ui.LocalSuccessAlert
 import com.example.foodics.ui.util.ObserveAsEffect
 import org.koin.androidx.compose.koinViewModel
 
@@ -12,10 +15,15 @@ fun TablesRoot(
 ) {
     val screenState by viewModel.screenState.collectAsStateWithLifecycle()
 
+    val errorAlertState = LocalErrorAlert.current
+    val successAlertState = LocalSuccessAlert.current
+
+    val context = LocalContext.current
+
     ObserveAsEffect(viewModel.effect) { effect ->
         when (effect) {
             is TablesScreenEffect.ShowError -> {
-                TODO()
+                errorAlertState.show(effect.message.asString(context))
             }
         }
     }
