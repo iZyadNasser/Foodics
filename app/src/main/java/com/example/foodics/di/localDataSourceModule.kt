368a89fcf2ce09dc0d7a5ee63a9275835f8bd8bf
Zyad <zyadhammad531@gmail.com>
2026-01-23 02:23:19 +0200

    feat: add koin dependencies for room

--- app/src/main/java/com/example/foodics/di/localDataSourceModule.kt
+++ app/src/main/java/com/example/foodics/di/localDataSourceModule.kt
@@ -1,7 +1,22 @@
 package com.example.foodics.di
 
+import androidx.room.Room
+import com.example.foodics.data.local.AppDatabase
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val localDataSourceModule = module {
-    // TODO
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            AppDatabase::class.java,
+            "foodics_database"
+        )
+            .fallbackToDestructiveMigration(false)
+            .build()
+    }
+
+    single { get<AppDatabase>().categoryDao() }
+    single { get<AppDatabase>().productDao() }
+    single { get<AppDatabase>().cartDao() }
 }
