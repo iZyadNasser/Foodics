a4136118c13a4f38c9ebcbb08cb334e5f5d3a186
Zyad <zyadhammad531@gmail.com>
2026-01-24 05:16:28 +0200

    fix: dependency versions conflict, and add indices to product entity

--- app/src/main/java/com/example/foodics/data/local/entity/ProductEntity.kt
+++ app/src/main/java/com/example/foodics/data/local/entity/ProductEntity.kt
@@ -2,6 +2,7 @@ package com.example.foodics.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
@@ -14,6 +15,9 @@ import androidx.room.PrimaryKey
             onDelete = ForeignKey.CASCADE
         )
     ],
+    indices = [
+        Index(value = ["categoryId"])
+    ]
 )
 data class ProductEntity(
     @PrimaryKey val id: String,
