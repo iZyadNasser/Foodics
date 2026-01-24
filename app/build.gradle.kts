fc8d14b0bbca6b275a97202c0b4b3d01b070e5d0
Zyad <zyadhammad531@gmail.com>
2026-01-24 21:26:05 +0200

    fix: agp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,9 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.foodics"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.foodics"
--- app/src/main/java/com/example/foodics/data/remote/exception/DataSourceException.kt
+++ app/src/main/java/com/example/foodics/data/remote/exception/DataSourceException.kt
@@ -9,7 +9,6 @@ class UnauthorizedApiException(errorMessage: String = "") : ApiErrorException(er
 class ForbiddenException(errorMessage: String = "") : ApiErrorException(errorMessage)
 class NotFoundException(errorMessage: String = "") : ApiErrorException(errorMessage)
 class ServerException(errorMessage: String = "") : ApiErrorException(errorMessage)
-class UnknownApiErrorException(errorMessage: String = "") : ApiErrorException(errorMessage)
 
 // General
 class DataSerializationException(errorMessage: String = "") : DataSourceException(errorMessage)
