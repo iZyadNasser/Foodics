0d24cae7b9b5b172c2f8034d6c91c69e16ea9fb4
Zyad <zyadhammad531@gmail.com>
2026-01-24 05:20:47 +0200

    feat: add ktor client provider

--- app/src/main/java/com/example/foodics/di/remoteDataSourceModule.kt
+++ app/src/main/java/com/example/foodics/di/remoteDataSourceModule.kt
@@ -4,12 +4,14 @@ import com.example.foodics.data.remote.source.CategoryRemoteDataSourceImpl
 import com.example.foodics.data.remote.source.ProductRemoteDataSourceImpl
 import com.example.foodics.data.repository.category.CategoryRemoteDataSource
 import com.example.foodics.data.repository.product.ProductRemoteDataSource
+import com.example.foodics.data.util.KtorClientProvider
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val remoteDataSourceModule = module {
+    single { KtorClientProvider.createHttpClient() }
     single(named("apiBaseUrl")) { "https://api.mockaroo.com" }
     single(named("apiNumber")) { "7f2d9600" }
     single(named("apiKey")) { "b418af70" }
